c12346a3e8b55ce6581622d6adb81a140078fd95
Andres Ponce <andreseeponce@gmail.com>
2024-09-09 22:13:05 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import asyncio
+import logging
+
+
+def init_logger():
+    pass
+
+
+def main():
+    """Initial function called on startup.
+    Process the Excel document and fill in the fields on
+    the required word docs."""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
